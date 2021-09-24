833aaeb308fb7de3b9e2e8d4682eab6da61be36b
Reynaldo Victor Arceo <iscreynaldovictor@gmail.com>
2021-09-24 18:39:13 -0500

    Create AccesoDatosEx.java

---
+++ src/sphinx/sicopese/excepciones/AccesoDatosEx.java
@@ -0,0 +1,15 @@
+
+package sphinx.sicopese.excepciones;
+
+
+public class AccesoDatosEx extends Exception{
+
+    
+    public AccesoDatosEx() {
+    }
+
+   
+    public AccesoDatosEx(String msg) {
+        super(msg);
+    }
+}
